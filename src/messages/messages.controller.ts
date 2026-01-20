acf8f3f6073f0aee8920264b63c647e9ee19c53b
Lucas Lima <lluuccaassfm@gmail.com>
2026-01-20 14:14:32 -0300

    First commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  findAll(@Query() pagination: any) {
+    return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return this.messagesService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.messagesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.messagesService.remove(id);
+  }
+}
---
+++ src/messages/messages.service.ts
@@ -0,0 +1,78 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+  private lastId = 1;
+  
+  private messages: Message[] = [
+    {
+      id: 1,
+      texto: 'Este é um recado de teste',
+      de: 'Joana',
+      para: 'João',
+      lido: false,
+      data: new Date(),
+    },
+  ];
+
+  throwNotFountError() {
+    throw new NotFoundException('Message not found');
+  }
+
+  findAll() {
+    return this.messages;
+  }
+
+  findOne(id: string) {
+    const message = this.messages.find((item) => item.id === +id);
+
+    if (message) return message;
+
+    this.throwNotFountError();
+  }
+
+  create(body: any) {
+    this.lastId++;
+    const id = this.lastId;
+    const novoRecado = {
+      id,
+      ...body,
+    };
+    this.messages.push(novoRecado);
+
+    return novoRecado;
+  }
+
+  update(id: string, body: any) {
+    const recadoExistenteIndex = this.messages.findIndex(
+      (item) => item.id === +id,
+    );
+
+    if (recadoExistenteIndex < 0) {
+      this.throwNotFountError();
+    }
+
+    if (recadoExistenteIndex >= 0) {
+      const recadoExistente = this.messages[recadoExistenteIndex];
+
+      this.messages[recadoExistenteIndex] = {
+        ...recadoExistente,
+        ...body,
+      };
+    }
+  }
+
+  remove(id: string) {
+    const recadoExistenteIndex = this.messages.findIndex(
+      item => item.id === +id,
+    );
+    
+    if (recadoExistenteIndex < 0) {
+      this.throwNotFountError();
+    }
+
+    this.messages.splice(recadoExistenteIndex, 1);
+    return `Recado ${id} removido com sucesso!`;
+  }
+}
